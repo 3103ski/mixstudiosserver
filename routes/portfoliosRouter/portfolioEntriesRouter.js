aaf522a30d06156070ca8b0813d301d6527e9fd0
3103ski <bjastski@gmail.com>
2021-04-14 00:38:16 -0700

    updates portfolio endpoint to send audio files to s3 bucket on aws

--- routes/portfoliosRouter/portfolioEntriesRouter.js
+++ routes/portfoliosRouter/portfolioEntriesRouter.js
@@ -5,51 +5,101 @@ const auth = require('../../authenticate');
 const cors = require('../cors');
 const PortfolioEntry = require('../../models/portfolios/portfolioEntries');
 
-const portfolioEntryRouter = express.Router();
+const AWS = require('aws-sdk');
+const FileType = require('file-type');
+const multiparty = require('multiparty');
+const fs = require('fs');
+const { response } = require('../../app');
 
-const audioStorage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, 'public/audio');
-	},
+AWS.config.update({
+	accessKeyId: 'AKIARRLEMNTVQBYT73P7',
+	secretAccessKey: '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',
 });
 
-const audioUpload = multer({ storage: audioStorage });
+const s3 = new AWS.S3();
+
+const portfolioEntryRouter = express.Router();
+
+const uploadFile = (buffer, name, type) => {
+	const params = {
+		ACL: 'public-read',
+		ContentType: type.mime,
+		Body: buffer,
+		Bucket: 'ms-audio-storage',
+		Key: `${name}.${type.ext}`,
+	};
+	console.log('these are the params: ', params);
+	return s3.upload(params).promise();
+};
 
 portfolioEntryRouter
 	.route('/')
 	.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
-	.post(
-		cors.corsWithOptions,
-		auth.verifyUser,
-		audioUpload.array('audio/wav'),
-		(req, res, next) => {
-			let portfolioEntry = {
-				userId: req.user._id,
-				title: req.body.title,
-				description: req.body.description,
-				genreOne: req.body.genreOne,
-				genreTwo: req.body.genreTwo,
-				category: req.body.category,
-				serviceAssignment: req.body.serviceAssignment,
-				image: req.body.image ? req.body.image : null,
-				isBeforeAfter: req.body.isBeforeAfter,
-				audioOne: req.files[0].filename,
-				audioOneOriginalName: req.files[0].originalname,
-				audioTwo: req.files[1] ? req.files[1].filename : null,
-				audioTwoOriginalName: req.files[1] ? req.files[1].originalname : null,
+	.post(cors.corsWithOptions, auth.verifyUser, (request, response, next) => {
+		const form = new multiparty.Form();
+
+		form.parse(request, async (error, fields, files) => {
+			if (error) {
+				response.status = 500;
+				response.setHeader('Content-Type', 'application/json');
+				response.json(error);
+			}
+
+			const path = files.audioOne[0].path;
+			const buffer = fs.readFileSync(path);
+			const type = await FileType.fromBuffer(buffer);
+			const fileName = `${Date.now().toString()}${files.audioOne[0].size}`;
+
+			console.log('is this a type? ', type.mime);
+
+			let payload = await {
+				userId: request.user._id,
+				title: fields.title[0],
+				description: fields.description[0],
+				genreOne: fields.genreOne[0],
+				genreTwo: fields.genreTwo[0],
+				audioOne: null,
+				audioTwo: null,
+				audioOneOriginalName: files.audioOne[0].originalFilename,
+				audioTwoOriginalName: null,
+				category: fields.category[0],
+				isBeforeAfter: fields.isBeforeAfter[0],
 			};
-			portfolioEntry.userId = req.user._id;
-			console.log(req.files);
-			console.log(portfolioEntry);
-			PortfolioEntry.create(portfolioEntry)
-				.then((newPortfolioObject) => {
-					res.status = 200;
-					res.setHeader('Content-Type', 'application/json');
-					res.json(newPortfolioObject);
+
+			uploadFile(buffer, fileName, type)
+				.then(async (fileOneResponse) => {
+					payload.audioOne = fileOneResponse.Key;
+
+					if (files.audioTwo) {
+						console.log('we had a second file to upload');
+						payload.audioTwoOriginalName = files.audioTwo[0].originalFilename;
+
+						const pathTwo = files.audioTwo[0].path;
+						const bufferTwo = fs.readFileSync(pathTwo);
+						const typeTwo = await FileType.fromBuffer(bufferTwo);
+						const fileNameTwo = `${Date.now().toString()}${files.audioTwo[0].size}`;
+
+						await uploadFile(bufferTwo, fileNameTwo, typeTwo).then(
+							async (fileTwoResponse) => {
+								payload.audioTwo = fileTwoResponse.Key;
+								console.log('Sending this DOUBLE SONG payload: ', payload);
+								return PortfolioEntry.create(payload);
+							}
+						);
+					} else {
+						console.log('Sending this SINGLE SONG payload: ', payload);
+						return PortfolioEntry.create(payload);
+					}
+				})
+				.then((newEntry) => {
+					console.log('SENDING THIS BACK: ', newEntry);
+					response.status = 200;
+					response.setHeader('Content-Type', 'application/json');
+					response.json(newEntry);
 				})
 				.catch((err) => next(err));
-		}
-	)
+		});
+	})
 	.get(cors.cors, (req, res, next) => {
 		PortfolioEntry.find()
 			.then((allPortfolioEntries) => {
