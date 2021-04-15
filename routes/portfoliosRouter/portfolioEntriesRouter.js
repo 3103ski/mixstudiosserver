8437f64935d1d6ab7af603264901ac67d3689a8f
3103ski <bjastski@gmail.com>
2021-04-14 20:03:04 -0700

    yet more cors testing

--- routes/portfoliosRouter/portfolioEntriesRouter.js
+++ routes/portfoliosRouter/portfolioEntriesRouter.js
@@ -36,6 +36,10 @@ portfolioEntryRouter
 	.options((req, res) => {
 		res.header('Access-Control-Allow-Origin', '*');
 		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+		res.header(
+			'Access-Control-Allow-Headers',
+			'Content-Type, Authorization, Content-Length, X-Requested-With, Access-Control-Allow-Origin'
+		);
 		res.sendStatus(200);
 	})
 	.post(cors.corsWithOptions, (req, res, next) => {
