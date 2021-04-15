47be8ac3b7ee1e3169c65f9708fcd89835982fb8
3103ski <bjastski@gmail.com>
2021-04-14 19:40:17 -0700

    yet more cors testing

--- routes/portfoliosRouter/portfolioEntriesRouter.js
+++ routes/portfoliosRouter/portfolioEntriesRouter.js
@@ -10,26 +10,26 @@ const FileType = require('file-type');
 const multiparty = require('multiparty');
 const fs = require('fs');
 
-// AWS.config.update({
-// 	accessKeyId: 'AKIARRLEMNTVQBYT73P7',
-// 	secretAccessKey: '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',
-// });
+AWS.config.update({
+	accessKeyId: 'AKIARRLEMNTVQBYT73P7',
+	secretAccessKey: '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',
+});
 
 // const s3 = new AWS.S3();
 
 const portfolioEntryRouter = express.Router();
 
-// const uploadFile = (buffer, name, type) => {
-// 	const params = {
-// 		ACL: 'public-read',
-// 		ContentType: type.mime,
-// 		Body: buffer,
-// 		Bucket: 'ms-audio-storage',
-// 		Key: `${name}.${type.ext}`,
-// 	};
-// 	console.log('these are the params: ', params);
-// 	return s3.upload(params).promise();
-// };
+const uploadFile = (buffer, name, type) => {
+	const params = {
+		ACL: 'public-read',
+		ContentType: type.mime,
+		Body: buffer,
+		Bucket: 'ms-audio-storage',
+		Key: `${name}.${type.ext}`,
+	};
+	console.log('these are the params: ', params);
+	return s3.upload(params).promise();
+};
 
 portfolioEntryRouter
 	.route('/')
@@ -38,74 +38,74 @@ portfolioEntryRouter
 		const form = new multiparty.Form();
 		console.log('We in the post');
 
-		res.status = 200;
-		res.setHeader('Content-Type', 'application/json');
-		res.setHeader('Access-Control-Allow-Origin', '*');
-		res.json({ return: 'everything kinda works?' });
+		// res.status = 200;
+		// res.setHeader('Content-Type', 'application/json');
+		// res.setHeader('Access-Control-Allow-Origin', '*');
+		// res.json({ return: 'everything kinda works?' });
 
-		// form.parse(req, async (error, fields, files) => {
-		// 	if (error) {
-		// 		response.status = 500;
-		// 		response.setHeader('Content-Type', 'application/json');
-		// 		response.json(error);
-		// 	}
-		// 	console.log('We past the first error check inside parse function');
+		form.parse(req, async (error, fields, files) => {
+			if (error) {
+				response.status = 500;
+				response.setHeader('Content-Type', 'application/json');
+				response.json(error);
+			}
+			console.log('We past the first error check inside parse function');
 
-		// 	const path = files.audioOne[0].path;
-		// 	const buffer = fs.readFileSync(path);
-		// 	const type = await FileType.fromBuffer(buffer);
-		// 	const fileName = `${Date.now().toString()}${files.audioOne[0].size}`;
+			const path = files.audioOne[0].path;
+			const buffer = fs.readFileSync(path);
+			const type = await FileType.fromBuffer(buffer);
+			const fileName = `${Date.now().toString()}${files.audioOne[0].size}`;
 
-		// 	let payload = await {
-		// 		userId: req.user._id,
-		// 		title: fields.title[0],
-		// 		description: fields.description[0],
-		// 		genreOne: fields.genreOne[0],
-		// 		genreTwo: fields.genreTwo[0],
-		// 		audioOne: null,
-		// 		audioTwo: null,
-		// 		audioOneOriginalName: files.audioOne[0].originalFilename,
-		// 		audioTwoOriginalName: null,
-		// 		category: fields.category[0],
-		// 		isBeforeAfter: fields.isBeforeAfter[0],
-		// 	};
+			let payload = await {
+				userId: req.user._id,
+				title: fields.title[0],
+				description: fields.description[0],
+				genreOne: fields.genreOne[0],
+				genreTwo: fields.genreTwo[0],
+				audioOne: null,
+				audioTwo: null,
+				audioOneOriginalName: files.audioOne[0].originalFilename,
+				audioTwoOriginalName: null,
+				category: fields.category[0],
+				isBeforeAfter: fields.isBeforeAfter[0],
+			};
 
-		// 	uploadFile(buffer, fileName, type)
-		// 		.then(async (fileOneResponse) => {
-		// 			payload.audioOne = fileOneResponse.Key;
+			uploadFile(buffer, fileName, type)
+				.then(async (fileOneResponse) => {
+					payload.audioOne = fileOneResponse.Key;
 
-		// 			if (files.audioTwo) {
-		// 				console.log('we had a second file to upload');
-		// 				payload.audioTwoOriginalName = files.audioTwo[0].originalFilename;
+					if (files.audioTwo) {
+						console.log('we had a second file to upload');
+						payload.audioTwoOriginalName = files.audioTwo[0].originalFilename;
 
-		// 				const pathTwo = files.audioTwo[0].path;
-		// 				const bufferTwo = fs.readFileSync(pathTwo);
-		// 				const typeTwo = await FileType.fromBuffer(bufferTwo);
-		// 				const fileNameTwo = `${Date.now().toString()}${files.audioTwo[0].size}`;
+						const pathTwo = files.audioTwo[0].path;
+						const bufferTwo = fs.readFileSync(pathTwo);
+						const typeTwo = await FileType.fromBuffer(bufferTwo);
+						const fileNameTwo = `${Date.now().toString()}${files.audioTwo[0].size}`;
 
-		// 				await uploadFile(bufferTwo, fileNameTwo, typeTwo).then(
-		// 					async (fileTwoResponse) => {
-		// 						payload.audioTwo = fileTwoResponse.Key;
-		// 						console.log('Sending this DOUBLE SONG payload: ', payload);
-		// 						return PortfolioEntry.create(payload);
-		// 					}
-		// 				);
-		// 			} else {
-		// 				console.log('Sending this SINGLE SONG payload: ', payload);
-		// 				return PortfolioEntry.create(payload);
-		// 			}
-		// 		})
-		// 		.then((newEntry) => {
-		// 			console.log('SENDING THIS BACK: ', newEntry);
-		// 			response.status = 200;
-		// 			res.setHeader('Content-Type', 'application/json');
-		// 			res.setHeader('Access-Control-Allow-Origin', '*');
+						await uploadFile(bufferTwo, fileNameTwo, typeTwo).then(
+							async (fileTwoResponse) => {
+								payload.audioTwo = fileTwoResponse.Key;
+								console.log('Sending this DOUBLE SONG payload: ', payload);
+								return PortfolioEntry.create(payload);
+							}
+						);
+					} else {
+						console.log('Sending this SINGLE SONG payload: ', payload);
+						return PortfolioEntry.create(payload);
+					}
+				})
+				.then((newEntry) => {
+					console.log('SENDING THIS BACK: ', newEntry);
+					response.status = 200;
+					res.setHeader('Content-Type', 'application/json');
+					res.setHeader('Access-Control-Allow-Origin', '*');
 
-		// 			// res.json({ return: 'everything kinda works?' });
-		// 			response.json(newEntry);
-		// 		})
-		// 		.catch((err) => next(err));
-		// });
+					// res.json({ return: 'everything kinda works?' });
+					response.json(newEntry);
+				})
+				.catch((err) => next(err));
+		});
 	})
 	.get(cors.cors, (req, res, next) => {
 		PortfolioEntry.find()
